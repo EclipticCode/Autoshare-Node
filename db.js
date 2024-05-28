fec9f3c603466d8c13990ef5c0ccf4ec844e24cd
daylightcoderj6 <jananibharathi6@gmail.com>
2024-05-28 18:34:04 +0530

    moved mongoDbUrl to .env file

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
-const mongoDbUrl = "mongodb+srv://jananib6:Janani2000@cluster0.4h2ombp.mongodb.net/AutoShare"
+const mongoDbUrl = process.env.mongoDbUrl;
 
 const connectDb = async () => {
    if(mongoose.connection.readyState === 1) return; 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 
 const {connectDb , mongoose} = require('./db')
 const {handleLogin , handleRegistration , handleCreateBooking , handleMyBookings , handleCancelBooking } = require('./service')
